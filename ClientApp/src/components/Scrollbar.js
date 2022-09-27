cde31c0a97bd946228e4064f95f1c2b8ae3bc0a7
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-09-27 05:44:06 -0700

    Changed backgroundColor at SimpleBarStyle

--- ClientApp/src/components/Scrollbar.js
+++ ClientApp/src/components/Scrollbar.js
@@ -14,6 +14,7 @@ const RootStyle = styled('div')(() => ({
 
 const SimpleBarStyle = styled(SimpleBarReact)(({ theme }) => ({
   maxHeight: '100%',
+  backgroundColor: theme.palette.chickadeeBeige.main,
   '& .simplebar-scrollbar': {
     '&:before': {
       backgroundColor: alpha(theme.palette.grey[600], 0.48),
