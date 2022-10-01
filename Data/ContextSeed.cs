1f42eab1d70a2ab4ac08e705f1ea02224f9e002c
Sunwoo Park <swparkaust@me.com>
2022-10-01 14:32:22 -0700

    Seed Default Super Admin user

--- Data/ContextSeed.cs
+++ Data/ContextSeed.cs
@@ -14,6 +14,33 @@ namespace chickadee.Data
             await roleManager.CreateAsync(new IdentityRole(Enums.Roles.PropertyManager.ToString()));
             await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Tenant.ToString()));
         }
+
+        public static async Task SeedSuperAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "superadmin@chickadeeinvest.ca",
+                Email = "superadmin@chickadeeinvest.ca",
+                FirstName = "Matt",
+                LastName = "Hardwick",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "ytyv)9kSBXmg");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Tenant.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.PropertyManager.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.SuperAdmin.ToString());
+                }
+
+            }
+        }
     }
 }
 
