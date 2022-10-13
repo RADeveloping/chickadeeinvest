46d48d890d4cdfd235e794cb02cd8f3fe0e1e561
RADeveloping <60758514+RADeveloping@users.noreply.github.com>
2022-10-12 19:02:49 -0700

    added an api route to get current user information

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using chickadee.Data;
+using chickadee.Models;
+
+namespace chickadee.Controllers
+{
+ 
+    using Microsoft.AspNetCore.Identity;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public  IActionResult GetUser()
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+            
+            if (_signInManager.IsSignedIn(User) && user != null)
+            {
+                var simpleUser = new
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    ProfilePicture = user.ProfilePicture,
+                    Id = user.Id,
+                    Email = user.Email,
+                };
+                
+                
+                return Ok(simpleUser);
+
+            };
+            
+            return NoContent();
+
+        }
+
+    }
+}
