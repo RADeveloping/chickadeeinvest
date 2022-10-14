391bb8513379f788c42849f1f2b563313788e145
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-14 01:22:14 -0700

    Added Controllers

---
+++ Controllers/UnitController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using chickadee.Data;
+using chickadee.Models;
+
+namespace chickadee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Unit
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Unit>>> GetUnits()
+        {
+          if (_context.Units == null)
+          {
+              return NotFound();
+          }
+            return await _context.Units.ToListAsync();
+        }
+
+        // GET: api/Unit/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Unit>> GetUnit(int id)
+        {
+          if (_context.Units == null)
+          {
+              return NotFound();
+          }
+            var unit = await _context.Units.FindAsync(id);
+
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return unit;
+        }
+
+        // PUT: api/Unit/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUnit(int id, Unit unit)
+        {
+            if (id != unit.UnitId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(unit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UnitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Unit
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Unit>> PostUnit(Unit unit)
+        {
+          if (_context.Units == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Units'  is null.");
+          }
+            _context.Units.Add(unit);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUnit", new { id = unit.UnitId }, unit);
+        }
+
+        // DELETE: api/Unit/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            if (_context.Units == null)
+            {
+                return NotFound();
+            }
+            var unit = await _context.Units.FindAsync(id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Units.Remove(unit);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UnitExists(int id)
+        {
+            return (_context.Units?.Any(e => e.UnitId == id)).GetValueOrDefault();
+        }
+    }
+}
