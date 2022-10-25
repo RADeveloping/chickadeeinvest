2a1c96feab4127a01a4de598a90169bc0edd036a
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-24 22:49:44 -0700

    Removed unused imports and commented out segments

--- ClientApp/src/pages/DashboardApp.js
+++ ClientApp/src/pages/DashboardApp.js
@@ -1,21 +1,17 @@
 // @mui
-import { useTheme } from '@mui/material/styles';
 import { Container, Typography } from '@mui/material';
 // components
 import Page from '../components/Page';
-import { useEffect, useState } from 'react';
 import useFetch from '../components/FetchData';
 import TicketList from '../components/TicketList';
 
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
-  // const [ticketData, setTicketData] = useState([]);
   const url = '/api/Ticket';
   const { data, error } = useFetch(url);
 
   const openTickets = data.filter((ticket) => ticket.status === 0);
-  // const url = "/api/Ticket";
 
   console.log(data);
   if (!data) {
