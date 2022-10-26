f25d6d51178700ff3399a0d3bb5962b9f1e1e2f0
Jong Hwan Seo <jseo51@my.bcit.ca>
2022-10-25 19:34:10 -0700

    Added controllers with correct names - same logic as the removed ones

---
+++ Controllers/PropertiesController.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using chickadee.Data;
+using chickadee.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace chickadee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PropertiesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Properties
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Property>>> GetProperties()
+        {
+          if (_context.Properties == null)
+          {
+              return NotFound();
+          }
+
+          var user = _userManager.GetUserAsync(User).Result;
+
+          var isTenant = await _userManager.IsInRoleAsync(user, "Tenant");
+
+          if (isTenant)
+          {
+            return NoContent();
+          }
+            return await _context.Properties
+              .Include(u => u.Units)
+                .ThenInclude(t => t.Tickets)
+              .Include(p => p.PropertyManager)
+              .ToListAsync();
+        }
+
+         // GET: api/Properties/current
+        [HttpGet]
+        [Route("current")]
+        public async Task<ActionResult<IEnumerable<Property>>> GetSpecificPropertyForUser()
+        {
+          if (_context.Properties == null)
+          {
+              return NotFound();
+          }
+            var user = _userManager.GetUserAsync(User).Result;
+
+            return await _context.Properties
+              .Include(j => j.PropertyManager)
+              .Where(t => t.PropertyManager == user)
+              .Include(t => t.Units)
+                .ThenInclude(s => s.Tickets)
+              .ToListAsync();
+        }
+
+        // GET: api/Properties/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Property>> GetProperty(int id)
+        {
+          if (_context.Properties == null)
+          {
+              return NotFound();
+          }
+            var @property = await _context.Properties.FindAsync(id);
+
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return @property;
+        }
+
+        // PUT: api/Properties/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProperty(int id, Property @property)
+        {
+            if (id != @property.PropertyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@property).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Properties
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Property>> PostProperty(Property @property)
+        {
+          if (_context.Properties == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Properties'  is null.");
+          }
+            _context.Properties.Add(@property);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProperty", new { id = @property.PropertyId }, @property);
+        }
+
+        // DELETE: api/Properties/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProperty(int id)
+        {
+            if (_context.Properties == null)
+            {
+                return NotFound();
+            }
+            var @property = await _context.Properties.FindAsync(id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            _context.Properties.Remove(@property);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PropertyExists(int id)
+        {
+            return (_context.Properties?.Any(e => e.PropertyId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using chickadee.Data;
+using chickadee.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace chickadee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TicketsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Tickets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+          if (_context.Tickets == null)
+          {
+              return NotFound();
+          }
+
+          var user = _userManager.GetUserAsync(User).Result;
+
+          var isTenant = await _userManager.IsInRoleAsync(user, "Tenant");
+
+          var isPropertyManager = await _userManager.IsInRoleAsync(user, "PropertyManager");
+
+          if (isTenant && _context.Units != null)
+          {
+            var ticketList = new List<Ticket>();
+            var tenantUnits = await _context.Units
+              .Include(t => t.Tenants)
+              .Where(unit => unit.Tenants.Contains(user))
+              .Include(i => i.Tickets)
+              .Include(j => j.Property)
+              .ToListAsync();
+            tenantUnits.ForEach((unit) => {
+              unit.Tickets.ForEach((ticket) => {
+                ticketList.Add(ticket);
+              });
+            });
+            return ticketList;
+          }
+
+          if (isPropertyManager && _context.Properties != null)
+          {
+            var ticketList = new List<Ticket>();
+            var properties = await _context.Properties
+              .Include(j => j.PropertyManager)
+              .Where(t => t.PropertyManager == user)
+              .Include(t => t.Units)
+                .ThenInclude(s => s.Tickets)
+              .ToListAsync();
+            properties.ForEach((property) => {
+              property.Units.ForEach((unit) => {
+                unit.Tickets.ForEach((ticket) => {
+                  ticketList.Add(ticket);
+                });
+              });
+            });
+            return ticketList;
+          }
+            return await _context.Tickets.ToListAsync();
+        }
+
+        // GET: api/Tickets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicket(int id)
+        {
+          if (_context.Tickets == null)
+          {
+              return NotFound();
+          }
+            var ticket = await _context.Tickets.FindAsync(id);
+
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return ticket;
+        }
+
+        // PUT: api/Tickets/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTicket(int id, Ticket ticket)
+        {
+            if (id != ticket.TicketId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ticket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tickets
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Ticket>> PostTicket(Ticket ticket)
+        {
+          if (_context.Tickets == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Tickets'  is null.");
+          }
+            _context.Tickets.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTicket", new { id = ticket.TicketId }, ticket);
+        }
+
+        // DELETE: api/Tickets/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTicket(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return NotFound();
+            }
+            var ticket = await _context.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tickets.Remove(ticket);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketExists(int id)
+        {
+            return (_context.Tickets?.Any(e => e.TicketId == id)).GetValueOrDefault();
+        }
+    }
+}
