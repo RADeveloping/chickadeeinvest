7a0bd2832243993d759c23b0e38d4e69609c30b0
Sunwoo Park <swparkaust@me.com>
2022-10-31 12:37:22 -0700

    WIP: working on fixing HTTPS issue

--- Program.cs
+++ Program.cs
@@ -56,13 +56,13 @@ else
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseIdentityServer();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthorization();
 
 app.MapControllerRoute(
