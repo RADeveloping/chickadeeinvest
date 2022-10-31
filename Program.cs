46cf1b4b17ab2fd1e9b4359b1dae0493b9c1260d
Sunwoo Park <swparkaust@me.com>
2022-10-31 10:32:38 -0700

    WIP: working on fixing the DB error

--- Program.cs
+++ Program.cs
@@ -75,6 +75,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<ApplicationDbContext>();
+	context.Database.EnsureCreated();
     context.Database.Migrate();
 }
 
