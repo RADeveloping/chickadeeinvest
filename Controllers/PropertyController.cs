77e3878a7fd2e8b99870f261567e28450b092896
RADeveloping <60758514+RADeveloping@users.noreply.github.com>
2022-11-04 00:20:18 -0700

    updated access

--- Controllers/PropertyController.cs
+++ Controllers/PropertyController.cs
@@ -30,8 +30,8 @@ namespace chickadee.Controllers
         
 
         // GET: api/Properties
-        [HttpGet("sort/{sortOrder?}")]
-        public async Task<ActionResult<IEnumerable<Property>>> GetProperties(string? sortOrder)
+        [HttpGet("{sort?}/{sortOrder?}")]
+        public async Task<ActionResult<IEnumerable<Property>>> GetProperties(string? sort, string? sortOrder)
         {
             var requestingUser = await _userManager.GetUserAsync(User);
 
@@ -51,7 +51,7 @@ namespace chickadee.Controllers
               .ToList();
 
 
-          if (requestingUser == null) return Ok(anonymous);
+          if (requestingUser == null || User.IsInRole("Tenant")) return Ok(anonymous);
           
               
           var properties = _context.Property
