03d221bef178c8b8c6aefdaa2ba5168041c2c4df
RADeveloping <60758514+RADeveloping@users.noreply.github.com>
2022-11-06 23:25:57 -0800

    allowed any user to get simple unit information

--- Controllers/UnitsController.cs
+++ Controllers/UnitsController.cs
@@ -28,6 +28,7 @@ namespace chickadee.Controllers
 
         // GET: api/properties/{propertyId}/units
         [HttpGet]
+        [Authorize(Roles = "SuperAdmin, Admin, PropertyManager, Tenant")]
         public async Task<ActionResult<IEnumerable<Unit>>> GetUnits(string propertyId)
         {
             var requestingUser = await _userManager.GetUserAsync(User);
@@ -41,25 +42,35 @@ namespace chickadee.Controllers
             {
                 return NotFound();
             }
+            
+            var unitsSimple = _context.Unit
+                .Where(u => u.PropertyId == propertyId)
+                .Select(unit => new
+                {
+                    unitId = unit.UnitId,
+                    unitNo = unit.UnitNo,
+                    unitType = unit.UnitType,
+                }).ToList();
+
 
             var units = _context.Unit
-              .Where(u=> u.PropertyId == propertyId)
-              .Select(unit => new
-              {
-                  unitId = unit.UnitId,
-                  unitNo = unit.UnitNo,
-                  unitType = unit.UnitType,
-                  propertyId = unit.PropertyId,
-                  propertyManagerId = unit.PropertyManagerId,
-              })
-              .ToList();
-              
+                .Where(u => u.PropertyId == propertyId)
+                .Select(unit => new
+                {
+                    unitId = unit.UnitId,
+                    unitNo = unit.UnitNo,
+                    unitType = unit.UnitType,
+                    propertyId = unit.PropertyId,
+                    propertyManagerId = unit.PropertyManagerId,
+                }).ToList();
+               
+
             if (User.IsInRole("SuperAdmin") || units.Any(p => p.propertyManagerId == requestingUser.Id || p.unitId == requestingUser.UnitId))
             {
                 return Ok(units);
             }
-            
-            return NotFound();
+
+            return Ok(unitsSimple);
         }
 
         
