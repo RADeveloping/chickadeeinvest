6f3011a8f4b62f823cd471eddc5d8fd81c8d70d2
apsm100 <amritmanhas11@gmail.com>
2022-11-09 00:27:36 -0800

    Add searchrowresult component

---
+++ ClientApp/src/components/SearchRowResult.js
@@ -0,0 +1,32 @@
+﻿import {Grid} from "@mui/material";
+import SortControl from "./SortControl";
+import {propertyProperties} from "../utils/filters";
+import Property from "./Property";
+import * as React from "react";
+
+export default function SearchRowResult({
+                                            isDesktop,
+                                            title,
+                                            loadingSearch,
+                                            orderBy,
+                                            handleOrderByChange,
+                                            properties,
+                                            order,
+                                            handleOrderChange,
+                                            viewComponent,
+                                            data
+                                        }) {
+    return (
+        <Grid item container spacing={1} justifyContent={isDesktop ? undefined : 'center'}>
+            <Grid width={'100%'} item>
+                <SortControl title={title} loadingSearch={loadingSearch}
+                             orderBy={orderBy}
+                             handleOrderByChange={handleOrderByChange}
+                             properties={properties}
+                             order={order}
+                             handleOrderChange={handleOrderChange}/>
+            </Grid>
+            {data.map(viewComponent)}
+        </Grid>
+    )
+}
