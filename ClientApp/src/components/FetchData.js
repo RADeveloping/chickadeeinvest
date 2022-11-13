bd150dd2d2bea4855a723bc2f945d0cee498b87f
apsm100 <amritmanhas11@gmail.com>
2022-11-13 13:33:51 -0800

    Add signal and docs

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,6 +1,15 @@
 import {useState, useEffect} from 'react';
 
-const useFetch = (url, filter, reset) => {
+let controller = new AbortController();
+/**
+ * Global fetch that returns common hooks for fetching.
+ * @param url {string} URL to fetch.
+ * @param [filter = false] {(a:[])=>void} Method to filter data before return.
+ * @param [reset = false] {boolean} Resets loading on new fetch. 
+ * @param [signal = false] {boolean} Uses signal to abort last fetch. Use this when fetches should not be done simultaneously.
+ * @returns {[*[],string,boolean]}
+ */
+const useFetch = (url, filter, reset, signal) => {
     const [data, setData] = useState([]);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
@@ -8,8 +17,12 @@ const useFetch = (url, filter, reset) => {
     useEffect(() => {
         if (url) {
             if (reset) setLoading(true)
+            if (signal) {
+                controller.abort();
+                controller = new AbortController();
+            }
             console.log(url)
-            fetch(url)
+            fetch(url, {signal: controller.signal})
                 .then((res) => res.json())
                 .then((data) => {
                     if (!data) data = []
